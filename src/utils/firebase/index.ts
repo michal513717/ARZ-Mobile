ad687475b66889ba2aebd7428ade9f1cbf04d6b7
Dominik Kozieł <domino15973@gmail.com>
2024-05-07 01:29:18 +0200

    added AsyncStorage to firebase auth

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,8 +1,12 @@
 import { initializeApp } from "firebase/app";
-
-import { getAuth } from "firebase/auth";
-
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { firebaseConfig } from "../configs/firebaseConfig";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
+
+const auth = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
